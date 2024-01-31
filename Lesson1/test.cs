7d1eb8de5c574086606368c59b7ba0200cba128b
jamalhasanovv <camal.hasanovv@gmail.com>
2024-01-31 13:29:20 +0400

    Lesson1

---
+++ Lesson1/test.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Test{
+
+     class program{
+
+     public static void Main() { 
+     Console.WriteLine("Hello Academy");
+     Console.WriteLine("my name is Jamal");
+     Console.ReadLine();
+ 
+  }      
+
+ }
+
+}
